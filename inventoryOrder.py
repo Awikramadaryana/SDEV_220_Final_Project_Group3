3b56bdc52899582e9a8ef158f075c7a92a4061d0
Ardhika Wikramadaryana <Awikramadaryana@ivytech.edu>
2026-03-07 14:51:12 -0500

    Initial Window Layout - InventoryOrder

--- inventoryOrder.py
+++ inventoryOrder.py
@@ -6,12 +6,26 @@ root = tk.Tk()
 root.title('Tkinter Window - Center')
 root.geometry("500x500")
 
-
+#Label Declarations
 orderLabel = tk.Label(root, text="Create New Order")
+itemCode = tk.Label(root, text="Insert Item Code")
+itemQty = tk.Label(root, text="Insert Item Quantity")
 
-name_entry = ttk.Entry(root)
+#User Entry Declarations
+codeEntry = ttk.Entry(root)
+qtyEntry = ttk.Entry(root)
 
+#Variable calls
 orderLabel.pack()
-name_entry.pack()
+
+itemCode.pack()
+codeEntry.pack()
+
+itemQty.pack()
+qtyEntry.pack()
+
+
+
+
 ## open window
 root.mainloop()
