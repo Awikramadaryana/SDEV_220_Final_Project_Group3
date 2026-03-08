467e5af2cc91579e5d39662b2c9b7b7b2beb81dc
Ardhika Wikramadaryana <Awikramadaryana@ivytech.edu>
2026-03-07 20:17:12 -0500

    Inventory Order, name declarations and write fix

--- inventoryOrder.py
+++ inventoryOrder.py
@@ -3,54 +3,68 @@
 
 import tkinter as tk
 from tkinter import ttk
+from inventory_master import CafeInventory
 
 
 root = tk.Tk()
 root.title('Tkinter Window - Center')
 root.geometry("500x500")
 
+#declare string variables to store item details
+nameStr = tk.StringVar()
+codeStr = tk.StringVar()
+qtyStr = tk.StringVar()
+
 #Label Declarations
 orderLabel = tk.Label(root, text="Create New Order")
+nameLabel = tk.Label(root, text = "Enter Item name")
 itemCode = tk.Label(root, text="Insert Item Code")
 itemQty = tk.Label(root, text="Insert Item Quantity")
 
 #User Entry Declarations
-codeEntry = ttk.Entry(root)
-qtyEntry = ttk.Entry(root)
+nameEntry = ttk.Entry(root, textvariable = nameStr)
+codeEntry = ttk.Entry(root, textvariable = codeStr)
+qtyEntry = ttk.Entry(root, textvariable = qtyStr)
 
-#Variable calls
-orderLabel.pack()
-
-itemCode.pack()
-codeEntry.pack()
-
-itemQty.pack()
-qtyEntry.pack()
-
-def fetch(entry, label):
-    label['text'] = entry.get()
-
+#Function to submit currently held variables into txt file
 def submit():
-
-    item=itemCode.get()
-    qty=itemQty.get()
+    name = nameStr.get()
+    item=codeStr.get()
+    qty=qtyStr.get()
     
     #Write input to file
     with open("OrderList.txt", "a") as f:
         f.write(item)
     with open("OrderList.txt", "a") as f:
         f.write(qty)
+    with open("OrderList.txt", "a") as f:
+        f.write(name)
 
-    print("The item is : " + item)
+    print("The item is : " + name)
+    print("The item code : " + item)
     print("The Quantity is : " + qty)
     
-    itemCode.set("")
-    itemQty.set("")
+    
+    nameStr.set("")
+    codeStr.set("")
+    qtyStr.set("")
 
 
 #open and read the file after the appending:
 with open("orderList.txt") as f:
   print(f.read())
+
+#Variable calls
+orderLabel.pack()
+
+nameLabel.pack()
+nameEntry.pack()
+
+itemCode.pack()
+codeEntry.pack()
+
+itemQty.pack()
+qtyEntry.pack()
 
 #Submit button
 sub_btn=tk.Button(root,text = 'Submit', command = submit)
